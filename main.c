6a517174b6dbc11bef8f75acff7dc702f508cf1e
Gustavo RV <gustavorv102@gmail.com>
2024-11-08 09:20:55 -0300

    histograma

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-int imprime_histograma(int *vendas);
+void imprime_histograma(int *vendas, int num_categorias);
 int main(){
     int *vendas;
     int num_categorias;
     scanf("%d", &num_categorias);
     vendas=(int*) malloc(num_categorias*sizeof(int));
+    for(int i=0; i<num_categorias; i++){
+        scanf("%d", &vendas[i]);
+    }
+    imprime_histograma(vendas, num_categorias);
+    free(vendas);
     return 0;
 }
-int imprime_histograma(int *vendas, int num_categorias){
-    for(int i=0; i<vendas; i++){
-        
+void imprime_histograma(int *vendas, int num_categorias){
+    for(int i=0; i<num_categorias; i++){
+        printf("%d ", vendas[i]);
+        for(int j=0; j<vendas[i]; j++){
+            printf("*");
+        }
+        printf("\n");
     }
 }
