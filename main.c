4b15bf484ca9b2692c3e219383505348364ba3d0
Gustavo RV <gustavorv102@gmail.com>
2024-11-14 10:03:43 -0300

    git push

--- main.c
+++ main.c
@@ -5,6 +5,9 @@ int main(){
     int *vendas;
     int num_categorias;
     scanf("%d", &num_categorias);
+    if(num_categorias==0){
+        printf("'vazio'");
+    }
     vendas=(int*) malloc(num_categorias*sizeof(int));
     for(int i=0; i<num_categorias; i++){
         scanf("%d", &vendas[i]);
